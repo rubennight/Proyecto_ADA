2b36fc2705ee053f98b6e6770e9e609e4f600182
Rubén J. Navarro <133140934+rubennight@users.noreply.github.com>
2023-12-11 20:33:43 -0600

    Se agregan los métodos de ordenamiento. (Radix no funciona por algún motivo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+
 import './App.css'
+import { Box, Button, Collapse } from '@mui/material'
+import Ordenamientos from './components/Ordenamientos'
+
+function libro(id, idGenero ,nombre, autor, fechaDePublicacion, genero){
+  return {id, idGenero ,nombre, autor, fechaDePublicacion, genero}
+}
+
+const libros = [
+  libro(2, 1, "El Problema de los Tres Cuerpos", "Liu Cixin", 2008, "Ciencia ficción"),
+  libro(3, 1, "Segunda Opción", "Autor 2", 2020, "Ciencia ficción"),
+  libro(1, 2, "1984", "George Orwell", 1949, "Realismo mágico"),
+  libro(6, 2, "Otro Libro", "Autor 3", 2015, "Realismo mágico"),
+  libro(8, 3, "Sapiens: De animales a dioses", "Yuval Noah Harari", 2014, "Distopía"),
+  libro(7, 3, "Libro de Distopía", "Autor 4", 2005, "Distopía"),
+  libro(5, 4, "Matar a un ruiseñor", "Harper Lee", 1960, "Misterio, thriller"),
+  libro(10, 4, "Otro Thriller", "Autor 5", 2018, "Misterio, thriller"),
+  libro(4, 5, "El Alquimista", "Paulo Coelho", 1988, "Ficción, filosofía"),
+  libro(9, 5, "Breve historia del tiempo", "Stephen Hawking", 1988, "Divulgación científica")
+];
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [openLibros, setOpenLibros] = React.useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>¡Bienvenida maestra, Vanessa!</h1>
+      <h3>Aquí se encuentra el proyecto asignado  para la materia Análisis y Diseño de Algoritmos</h3>
+      
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <Box 
+        style={{
+          textAlign: 'left',
+          padding: 20,
+          backgroundColor: '#AEDEFC',
+          borderRadius: 30,
+          margin: 20 
+          }}
+      >
+        <h3>Semestre: 3ro</h3>
+        <h3>Grupo: D</h3>
+        <h3>Modalidad: Semiescolarizado</h3>
+        <h3>Profesor: Vanessa del Rosario Alcala Ramirez</h3>
+        <h3>Alumno: Rubén Jiménez Navarro</h3>         
+      </Box>
+      
+
+      <Box
+        style={{
+          textAlign: 'left',
+          padding: 20,
+          backgroundColor: '#FFE69A',
+          borderRadius: 30,
+          margin: 20  
+          }}
+      >
+        ¡Aquí podremos ver un conjunto de libros, personalmente, son mis favoritos!
+        <br />
+        <br />
+        <Button variant="text " onClick={() => setOpenLibros(!openLibros)}>Libros</Button>  
+        <br />
+        <br />
+        <Collapse in={openLibros}>
+          {libros.map((libro) => (
+            <div key={libro.id}>
+              <Box 
+                style={{
+                  border: '1px solid gray' ,
+                  borderRadius: 10,
+                  padding: 10
+                }}
+              >
+                Id: {libro.id}
+                <br />
+                Id del genero: {libro.idGenero}
+                <br />
+                Nombre: {libro.nombre}
+                <br />
+                Autor: {libro.autor}
+                <br />
+                Año de publicación: {libro.fechaDePublicacion}
+                <br />
+                Género: {libro.genero}
+              </Box>
+              <br />
+            </div>
+          ))}          
+        </Collapse>
+      </Box>
+      
+      <br />
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <Box
+        style={{
+          textAlign: 'left',
+          padding: 20,
+          backgroundColor: '#E8D3FF',
+          borderRadius: 30,
+          margin: 20  
+        }}
+      >
+        Ahora podemos organizar estos libros con los siguientes algoritmos: 
+        <br />
+        <br />
+        <Ordenamientos libros={libros} />
+
+      </Box>
+
+
+
+    </div>
   )
 }
 
---
+++ src/components/Ordenamientos.jsx
@@ -0,0 +1,363 @@
+  import { Button, Box } from '@mui/material';
+  import React, { useState } from 'react';
+
+  const Ordenamientos = ({ libros }) => {
+    const [sortedLibros, setSortedLibros] = useState([...libros]);
+
+    
+  const resetLibros = () => {
+    setSortedLibros([...libros]);
+  };
+
+    const bubbleSort = () => {
+      const n = sortedLibros.length;
+      let tempLibros = [...sortedLibros];
+      let swapped;
+
+      let i = 0;
+      let j = 0;
+
+      const sortStep = () => {
+        if (i < n - 1) {
+          if (tempLibros[i].id > tempLibros[i + 1].id) {
+            // Swap libros
+            [tempLibros[i], tempLibros[i + 1]] = [tempLibros[i + 1], tempLibros[i]];
+            swapped = true;
+
+            // Update state to trigger re-render
+            setSortedLibros([...tempLibros]);
+          }
+          i++;
+        } else {
+          i = 0;
+          if (!swapped) {
+            // Si no hubo intercambios, el arreglo está ordenado
+            setShowSteps(true);
+            return;
+          }
+          swapped = false;
+          j++;
+        }
+
+        // Siguiente iteración
+        setTimeout(sortStep, 1000); // Ajusta el retardo según sea necesario
+      };
+
+      // Comienza el proceso de ordenamiento
+      sortStep();
+    };
+
+    const insertionSort = () => {
+      let tempLibros = [...sortedLibros];
+
+      const performInsertionSort = async () => {
+        for (let i = 1; i < tempLibros.length; i++) {
+          let j = i - 1;
+          const currentLibro = tempLibros[i];
+
+          while (j >= 0 && tempLibros[j].id > currentLibro.id) {
+            // Swap libros
+            tempLibros[j + 1] = tempLibros[j];
+            tempLibros[j] = currentLibro;
+
+            // Update state to trigger re-render after each swap
+            setSortedLibros([...tempLibros]);
+
+            // Wait for 1000 miliseconds
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            j--;
+          }
+        }
+      };
+
+      // Start the insertion sort process
+      performInsertionSort();
+    };
+
+    const mergeSort = async () => {
+      const merge = (left, right) => {
+        let result = [];
+        let leftIndex = 0;
+        let rightIndex = 0;
+    
+        while (leftIndex < left.length && rightIndex < right.length) {
+          if (left[leftIndex].id < right[rightIndex].id) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+          } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+          }
+        }
+    
+        return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+      };
+    
+      const performMergeSort = async (arr) => {
+        const len = arr.length;
+        if (len <= 1) {
+          return arr;
+        }
+    
+        const middle = Math.floor(len / 2);
+        const left = arr.slice(0, middle);
+        const right = arr.slice(middle);
+    
+        const leftSorted = await performMergeSort(left);
+        const rightSorted = await performMergeSort(right);
+    
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    
+        return merge(leftSorted, rightSorted);
+      };
+    
+      const sortedArray = await performMergeSort(sortedLibros);
+    
+      // Update state to trigger re-render after sorting is complete
+      setSortedLibros([...sortedArray]);
+    };
+
+    const heapSort = async () => {
+      const heapify = async (arr, n, i) => {
+        let largest = i;
+        let left = 2 * i + 1;
+        let right = 2 * i + 2;
+
+        if (left < n && arr[left].id > arr[largest].id) {
+          largest = left;
+        }
+
+        if (right < n && arr[right].id > arr[largest].id) {
+          largest = right;
+        }
+
+        if (largest !== i) {
+          // Swap libros
+          [arr[i], arr[largest]] = [arr[largest], arr[i]];
+
+          // Update state to trigger re-render after each swap
+          setSortedLibros([...arr]);
+
+          // Wait for 1000 miliseconds
+          await new Promise(resolve => setTimeout(resolve, 1000));
+
+          await heapify(arr, n, largest);
+        }
+      };
+
+      const performHeapSort = async () => {
+        const n = sortedLibros.length;
+
+        for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+          await heapify(sortedLibros, n, i);
+        }
+
+        for (let i = n - 1; i > 0; i--) {
+          // Swap libros
+          [sortedLibros[0], sortedLibros[i]] = [sortedLibros[i], sortedLibros[0]];
+
+          // Update state to trigger re-render after each swap
+          setSortedLibros([...sortedLibros]);
+
+          // Wait for 1000 miliseconds
+          await new Promise(resolve => setTimeout(resolve, 1000));
+
+          await heapify(sortedLibros, i, 0);
+        }
+      };
+
+      // Start the heap sort process
+      await performHeapSort();
+    };
+
+    const quickSort = async () => {
+      const partition = async (arr, low, high) => {
+        const pivot = arr[high].id;
+        let i = low - 1;
+
+        for (let j = low; j < high; j++) {
+          if (arr[j].id < pivot) {
+            i++;
+            // Swap libros
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+
+            // Update state to trigger re-render after each swap
+            setSortedLibros([...arr]);
+
+            // Wait for 1000 miliseconds
+            await new Promise(resolve => setTimeout(resolve, 1000));
+          }
+        }
+
+        // Swap libros
+        [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+
+        // Update state to trigger re-render after each swap
+        setSortedLibros([...arr]);
+
+        // Wait for 1000 miliseconds
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        return i + 1;
+      };
+
+      const performQuickSort = async (arr, low, high) => {
+        if (low < high) {
+          const pi = await partition(arr, low, high);
+
+          await Promise.all([
+            performQuickSort(arr, low, pi - 1),
+            performQuickSort(arr, pi + 1, high)
+          ]);
+        }
+      };
+
+      // Start the quick sort process
+      await performQuickSort(sortedLibros, 0, sortedLibros.length - 1);
+    };
+    
+    const countingSort = async () => {
+      const maxId = Math.max(...sortedLibros.map(libro => libro.id));
+      const countArray = new Array(maxId + 1).fill(0);
+    
+      sortedLibros.forEach(libro => {
+        countArray[libro.id]++;
+      });
+    
+      let sortedArray = [];
+    
+      for (let i = 0; i < countArray.length; i++) {
+        // Agrega count veces el libro con el id al arreglo ordenado
+        for (let j = 0; j < countArray[i]; j++) {
+          sortedArray.push({ ...sortedLibros.find(libro => libro.id === i) });
+    
+          // Actualiza el arreglo ordenado y espera 1000 milisegundos
+          setSortedLibros(sortedArray.slice());
+          await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+      }
+    };
+
+    const bucketSort = async () => {
+      const maxId = Math.max(...sortedLibros.map(libro => libro.id));
+      const numBuckets = 5;
+    
+      const buckets = Array.from({ length: numBuckets }, () => []);
+    
+      sortedLibros.forEach(libro => {
+        const bucketIndex = Math.floor((libro.id / maxId) * (numBuckets - 1)); // Adjusted the bucket index calculation
+        buckets[bucketIndex].push(libro);
+      });
+    
+      let sortedArray = [];
+      for (let i = 0; i < numBuckets; i++) {
+        buckets[i] = buckets[i].sort((a, b) => a.id - b.id);
+        sortedArray = sortedArray.concat(buckets[i]);
+        setSortedLibros([...sortedArray]);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      }
+    };   
+
+    const radixSort = async () => {
+      const getMax = () => {
+        return Math.max(...sortedLibros.map(libro => libro.id));
+      };
+    
+      const countingSort = async (exp) => {
+        const n = sortedLibros.length;
+        const output = Array(n);
+        const count = Array(10).fill(0);
+    
+        sortedLibros.forEach(libro => {
+          const digit = Math.floor((libro.id / exp) % 10);
+          count[digit]++;
+        });
+    
+        for (let i = 1; i < 10; i++) {
+          count[i] += count[i - 1];
+        }
+    
+        for (let i = n - 1; i >= 0; i--) {
+          const digit = Math.floor((sortedLibros[i].id / exp) % 10);
+          output[count[digit] - 1] = { ...sortedLibros[i] };
+          count[digit]--;
+        }
+    
+        setSortedLibros(output);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      };
+    
+      const max = getMax();
+    
+      for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
+        await countingSort(exp);
+      }
+    };
+    
+    
+    
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row', padding: 10 }}>
+        <div>
+          <Button variant="contained" onClick={resetLibros}>
+            Restaurar Lista Original
+          </Button>  
+          <br />    
+          <br />    
+          <Button variant="contained" onClick={bubbleSort}>
+            Ordenar con Bubble Sort
+          </Button>
+          <br />
+          <br />    
+          <Button variant="contained" onClick={insertionSort}>
+            Ordenar con Insertion Sort
+          </Button>
+          <br />
+          <br />    
+          <Button variant="contained" onClick={mergeSort}>
+            Ordenar con Merge Sort
+          </Button>      
+          <br />
+          <br />    
+          <Button variant="contained" onClick={heapSort}>
+            Ordenar con Heap Sort
+          </Button> 
+          <br />
+          <br />
+          <Button variant="contained" onClick={quickSort}>
+              Ordenar con Quick Sort
+          </Button>
+          <br />
+          <br />
+          <Button variant="contained" onClick={countingSort}>
+            Ordenar con Counting Sort
+          </Button>
+          <br />
+          <br />
+          <Button variant="contained" onClick={bucketSort}>
+            Ordenar con Bucket Sort
+          </Button>
+          <br />
+          <br />
+          <Button variant="contained" onClick={radixSort}>
+            Ordenar con Radix Sort
+          </Button>
+        </div>
+
+        <Box mt={2} style={{ marginLeft: 50}}>
+          
+          {sortedLibros.map((libro) => (
+            <p key={libro.id}>
+              Id: {libro.id}
+              <br />
+              Nombre: {libro.nombre}
+            </p>
+          ))}
+        </Box>
+      </div>
+    );
+  };
+
+  export default Ordenamientos;
